125da32ea2086ad556cece764a0f83b3332f3712
Kazungu Derrick <kazunguderriq@gmail.com>
2022-11-25 16:23:54 +0300

    commit tree a3eeba9723509383cd7684312bcb5126273544c4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,5 @@
 class Post < ApplicationRecord
-    # Validations: add validations to Post such that: 
-    # 1. the title cannot be blank
-    validates :title, presence: true
-    # 2. the category is either "Fiction" or "Non-Fiction"
-    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
-    # 3. the content is at least 100 characters long
-    validates :content, length: { minimum: 100 }
+  validates :title, presence: true
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validates :content, length: { minimum: 100 }
 end
